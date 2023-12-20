a71d3a4ed6d9b179891000480ac4e1bde0aca299
Javid Hassani <91068797+Javidleo@users.noreply.github.com>
2023-12-20 14:07:35 +0330

    Adding new project for http client testing

---
+++ src/Feature/HttpClient/Client/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.ReadKey();
+
+var client = new HttpClient();
+
+client.BaseAddress = new Uri("https://localhost:7161");
+
+var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, "/api/User"));
+
+Console.ReadKey();
+
+var content = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(content);
+
---
+++ src/Feature/HttpClient/Server/Models/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Server.Models;
+
+public class UserInfo
+{
+    public UserInfo(int id, string name, string family)
+    {
+        Id = id;
+        Name = name;
+        Family = family;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+}

88dd036b89f698e086f4226cc0b797aa770cdeb4
Javid Hassani <91068797+Javidleo@users.noreply.github.com>
2024-02-15 17:49:06 +0330

    clean up

--- ClientSide/obj/Debug/net8.0/ClientSide.AssemblyInfo.cs
+++ ClientSide/obj/Debug/net8.0/ClientSide.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientSide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6378eb580463f48817ea3c88998d86ad81047429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientSide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientSide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
